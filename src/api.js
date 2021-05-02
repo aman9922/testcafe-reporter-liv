82d93d29b5fb23fc565048c34fb1bdec0acbbeb6
Aman Gupta <aman.gupta@livspace.com>
2021-05-02 18:27:57 +0530

    test

--- src/api.js
+++ src/api.js
@@ -3,9 +3,12 @@ const axios = require('axios');
 //const url = 'http://localhost:5000/posts/add';
 const url = process.env.DASHBOARD_URL + '/add';
 
-module.exports = function (projectName, environment, build, fixtureName, stepName, status, durationMs, screenShotArr, errorArr, tags) {
+module.exports = function (projectName, environment, build, fixtureName, stepName, status, durationMs) {
     try {
-        axios.post(url, { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr, 'tags': tags })
+        console.log(url + '*******************************');
+        // axios.post(url, { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr, 'tags': tags })
+        // .catch(err => console.log('Not able to post test through api', err));
+        axios.post(url, { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs })
             .catch(err => console.log('Not able to post test through api', err));
     }
     catch (error) {
